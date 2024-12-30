8bebbf562946ac06c23c1fb483631e985bfe9aea
Jack Steam <jacksteamdev@gmail.com>
2024-12-29 18:05:16 -0600

    feat: add version flag

--- packages/mcp-server/src/version.ts
+++ packages/mcp-server/src/version.ts
@@ -1,9 +1,15 @@
 import path from "path";
+import fs from "fs";
 
 export async function getVersion() {
-  const file = Bun.file(
-    path.resolve(import.meta.dir, "../../obsidian-plugin/package.json"),
-  );
-  const { version } = await file.json();
-  return version;
+  try {
+    const packageJson = path.resolve(
+      import.meta.dir,
+      "../../obsidian-plugin/package.json",
+    );
+    const { version } = JSON.parse(fs.readFileSync(packageJson, "utf-8"));
+    return version;
+  } catch (error) {
+    throw new Error(`Error getting version: ${error}`);
+  }
 }
