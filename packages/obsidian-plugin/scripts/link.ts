b62f45c2729a9e27fab3b0536e2b346cc867574f
Jack Steam <jacksteamdev@gmail.com>
2025-01-08 11:10:57 -0600

    fix(dev:link): use obsidian vault config folder instead of vault folder

--- packages/obsidian-plugin/scripts/link.ts
+++ packages/obsidian-plugin/scripts/link.ts
@@ -4,43 +4,46 @@ import { join, resolve } from "node:path";
 /**
  * This development script creates a symlink to the plugin in the Obsidian vault's plugin directory. This allows you to
  * develop the plugin in the repository and see the changes in Obsidian without having to manually copy the files.
- * 
- * It is not included in the plugin itself. It is only used to setup development.
- * 
+ *
+ * This function is not included in the plugin itself. It is only used to set up local development.
+ *
  * Usage: `bun scripts/link.ts <path_to_obsidian_vault>`
  * @returns {Promise<void>}
  */
 async function main() {
   const args = process.argv.slice(2);
   if (args.length < 1) {
-    console.error("Usage: bun scripts/link.ts <path_to_obsidian_vault>");
+    console.error(
+      "Usage: bun scripts/link.ts <path_to_obsidian_vault_config_folder>",
+    );
     process.exit(1);
   }
-  const vaultPath = args[0];
 
-  const manifestPath = resolve(__dirname, "../manifest.json");
-  const file = Bun.file(manifestPath);
+  const vaultConfigPath = args[0];
+  const projectRootDirectory = resolve(__dirname, "../../..");
+  const pluginManifestPath = resolve(projectRootDirectory, "manifest.json");
+  const pluginsDirectoryPath = join(vaultConfigPath, "plugins");
+
+  const file = Bun.file(pluginManifestPath);
   const manifest = await file.json();
 
-  const pluginsPath = join(vaultPath, ".obsidian", "plugins");
   const pluginName = manifest.id;
-  const sourcePath = resolve(__dirname, "..");
   console.log(
-    `Creating symlink to ${sourcePath} for plugin ${pluginName} in ${pluginsPath}`,
+    `Creating symlink to ${projectRootDirectory} for plugin ${pluginName} in ${pluginsDirectoryPath}`,
   );
 
-  if (!existsSync(pluginsPath)) {
-    mkdirSync(pluginsPath, { recursive: true });
+  if (!existsSync(pluginsDirectoryPath)) {
+    mkdirSync(pluginsDirectoryPath, { recursive: true });
   }
 
-  const targetPath = join(pluginsPath, pluginName);
+  const targetPath = join(pluginsDirectoryPath, pluginName);
 
   if (existsSync(targetPath)) {
     console.log("Symlink already exists.");
     return;
   }
 
-  symlinkSync(sourcePath, targetPath, "dir");
+  symlinkSync(projectRootDirectory, targetPath, "dir");
   console.log("Symlink created successfully.");
 
   console.log(
